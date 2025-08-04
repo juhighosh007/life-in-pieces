97f94af2400ae46f5b1ec1dfc042482ad2d3c511
Juhi <24372403-Juhig007@users.noreply.replit.com>
2025-08-04 07:41:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/* VARIABLES */
+
+/* PRELOAD LOADS FILES */
+function preload(){
+  
+}
+
+/* SETUP RUNS ONCE */
+function setup() {
+  createCanvas(400,400);
+
+}
+
+/* DRAW LOOP REPEATS */
+function draw() {
+
+}
+
+/* FUNCTIONS */
